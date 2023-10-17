54f514af9c60175ef7a888092ab54ea1fbf3c41e
michael-lesirge <100492377+michael-lesirge@users.noreply.github.com>
2023-10-16 17:20:18 -0700

    Add slow and boost modes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
@@ -1,5 +1,13 @@
 package org.firstinspires.ftc.teamcode;
 
 public class Constants {
-    // TODO store all motor names and directions in here
+
+    public static  final double armPower = 1;
+    public static final int armMoveAmount = 1;
+
+    public static final double[] moveSpeedModifiers = {0.25, 0.75, 1};
+    public static final double[] spinSpeedModifiers = {0.1, 0.5, 1};
+
+    public static final boolean shouldStartInSlow = false;
+    public static final boolean slowModeIsToggleMode = true;
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainTeleOp.java
@@ -5,37 +5,38 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.firstinspires.ftc.robotcore.external.Const;
+
 @TeleOp(name="Main Drive", group="Linear OpMode")
 public class MainTeleOp extends LinearOpMode {
 
   // Declare OpMode members for each of the 4 motors.
-  private ElapsedTime runtime = new ElapsedTime();
-  private DcMotor leftFrontDrive;
-  private DcMotor leftBackDrive;
-  private DcMotor rightFrontDrive;
-  private DcMotor rightBackDrive;
-
-  private DcMotor arm;
+  final private ElapsedTime runtime = new ElapsedTime();
 
   @Override
   public void runOpMode() {
 
     // get motors using set motor names
-    leftFrontDrive  = hardwareMap.get(DcMotor.class, "FL");
-    leftBackDrive  = hardwareMap.get(DcMotor.class, "BL");
-    rightFrontDrive = hardwareMap.get(DcMotor.class, "FR");
-    rightBackDrive = hardwareMap.get(DcMotor.class, "BR");
+    final DcMotor leftFrontDrive = hardwareMap.get(DcMotor.class, "FL");
+    final DcMotor leftBackDrive  = hardwareMap.get(DcMotor.class, "BL");
+    final DcMotor rightFrontDrive = hardwareMap.get(DcMotor.class, "FR");
+    final DcMotor rightBackDrive = hardwareMap.get(DcMotor.class, "BR");
 
-    // get arm
-    arm = hardwareMap.get(DcMotor.class, "top arm");
-    arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-    arm.setPower(0.5);
-
-    // set if motor is reversed
     leftFrontDrive.setDirection(DcMotor.Direction.REVERSE);
     leftBackDrive.setDirection(DcMotor.Direction.REVERSE);
     rightFrontDrive.setDirection(DcMotor.Direction.FORWARD);
     rightBackDrive.setDirection(DcMotor.Direction.FORWARD);
+
+    final DcMotor arm = hardwareMap.get(DcMotor.class, "top arm");
+
+    int targetArmPosition = arm.getCurrentPosition();
+
+    arm.setTargetPosition(targetArmPosition);
+    arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+    arm.setPower(Constants.armPower);
+
+    // set if motor is reversed
+
 
     // Wait for the game to start (driver presses PLAY)
     telemetry.addData("Status", "Initialized");
@@ -44,16 +45,28 @@ public class MainTeleOp extends LinearOpMode {
     waitForStart();
     runtime.reset();
 
-    int targetArmPosition = arm.getCurrentPosition();
+    boolean isSlowMode = Constants.shouldStartInSlow;
+    boolean lastBstate = false;
 
     // run until the end of the match (driver presses STOP)
     while (opModeIsActive()) {
       double max;
 
+      if (Constants.slowModeIsToggleMode) {
+        if (gamepad1.b && !lastBstate) isSlowMode = !isSlowMode;
+      }
+      else {
+        isSlowMode = gamepad1.b;
+      }
+      lastBstate = gamepad1.b;
+
       // left joystick to go forward & strafe, and right joystick to rotate.
-      double axial = -gamepad1.left_stick_y;
-      double lateral = gamepad1.left_stick_x;
-      double yaw = gamepad1.right_stick_x;
+      double axial = -gamepad1.left_stick_y * Constants.moveSpeedModifiers[1 + (gamepad1.left_stick_button ? 1 : 0) - (isSlowMode ? 1 : 0)];
+      double lateral = gamepad1.left_stick_x * Constants.moveSpeedModifiers[1 + (gamepad1.left_stick_button ? 1 : 0) - (isSlowMode ? 1 : 0)];
+      double yaw = gamepad1.right_stick_x * Constants.spinSpeedModifiers[1 + (gamepad1.right_stick_button ? 1 : 0) - (isSlowMode ? 1 : 0)];
+
+      if (gamepad1.dpad_down) targetArmPosition += Constants.armMoveAmount;
+      if (gamepad1.dpad_down) targetArmPosition -= Constants.armMoveAmount;
 
       // Combine joystick requests for each axis-motion to determine each wheel's power.
       double leftFrontPower = axial + lateral + yaw;
@@ -74,6 +87,8 @@ public class MainTeleOp extends LinearOpMode {
         rightBackPower /= max;
       }
 
+      arm.setTargetPosition(targetArmPosition);
+
       // Send power to wheels
       leftFrontDrive.setPower(leftFrontPower);
       rightFrontDrive.setPower(rightFrontPower);
@@ -81,14 +96,15 @@ public class MainTeleOp extends LinearOpMode {
       rightBackDrive.setPower(rightBackPower);
 
       // telemetry
-      telemetry.addData("Status", "Run Time: " + runtime.toString());
-      telemetry.addData("Front left/Right", "%4.2f, %4.2f", leftFrontPower, rightFrontPower);
-      telemetry.addData("Back  left/Right", "%4.2f, %4.2f", leftBackPower, rightBackPower);
+      telemetry.addData("Status", "Run Time: %s", runtime.toString());
+      telemetry.addData("Front Left/Right", "%4.2f, %4.2f", leftFrontPower, rightFrontPower);
+      telemetry.addData("Back  Left/Right", "%4.2f, %4.2f", leftBackPower, rightBackPower);
+
+      telemetry.addData("Arm   Target/Real", "%d, %d", targetArmPosition, arm.getCurrentPosition());
+
+      telemetry.addData("Slow Mode: ", String.valueOf(isSlowMode));
+
       telemetry.update();
 
-      if (gamepad1.dpad_down) targetArmPosition += 1;
-      if (gamepad1.dpad_down) targetArmPosition -= 1;
-
-      arm.setTargetPosition(targetArmPosition);
     }
   }}
