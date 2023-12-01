184afd336f2ec1ae9fb93c0a2c74fd9dd6b5212e
michael-lesirge <100492377+michael-lesirge@users.noreply.github.com>
2023-11-30 23:22:23 -0800

    lol

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoUtils.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode;
+
+/**
+ * I wanted to put all auto utils here, but I did not want to break currently working.
+ * I have not tried importing classes into other files on the robot and I did not want to try it and see it did not work.
+ *
+ * Sorry for the bad code!
+ */
+public class AutoUtils {
+}
